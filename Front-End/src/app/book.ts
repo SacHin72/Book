b8798f426554ccf41efb3ad29993c11d9b7b7e69
SacHin72 <37321934+SacHin72@users.noreply.github.com>
2020-07-25 14:59:42 +0530

    Book store

--- Front-End/src/app/book.ts
+++ Front-End/src/app/book.ts
@@ -1,6 +1,7 @@
 export class Book{
     bookId:number;
     title:string;
+    price:number;
     author:string;
     description:string;
     isbnNumber:string;
--- Front-End/src/app/update-book/update-book.component.ts
+++ Front-End/src/app/update-book/update-book.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../book'
 import { BookService } from '../book.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-update-book',
@@ -10,26 +11,30 @@ import { Router } from '@angular/router';
 })
 export class UpdateBookComponent implements OnInit {
   book :Book=new Book();
-  
-  constructor(private bookService:BookService,private router: Router) { }
-  message:String;
+  newBook :Book=new Book();
+  todayDate : Date = new Date();
+  successMessage;
+  errorMessage;
+  showSuccessMessage:boolean;
+  showErorrMessage:boolean;
+
+  constructor(private bookService:BookService,private route : ActivatedRoute) { }
+
   ngOnInit(): void {
   }
 
   updateBook(book:Book){
+    this.book.lastUpdateTime=this.todayDate;
     this.book.bookCategory=null;
-    console.log('Updating...Please Wait!!!');
     this.bookService.updateBook(book).subscribe(
-        data=>{
-          this.message=data;
-          console.log(this.message);
-          console.log(data);
-          alert(this.message);
-          
-        },
-        error => {
-          console.log(this.message);
-        }
+      data=>{
+        this.successMessage=data,
+        this.showSuccessMessage=true
+      },
+      (error:HttpErrorResponse)=>{
+        this.errorMessage=error.error.message,
+        this.showErorrMessage=true
+      }
     );
   }
 }
